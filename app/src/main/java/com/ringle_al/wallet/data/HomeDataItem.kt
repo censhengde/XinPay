87f8513a83b861767203bb194f038a0bbb6c49eb
censhengde <184665422@qq.com>
2019-12-09 19:26:38 +0800

    second commit

---
+++ app/src/main/java/com/ringle_al/wallet/data/HomeDataItem.kt
@@ -0,0 +1,15 @@
+package com.ringle_al.wallet.data
+
+/**
+ * create by 岑胜德
+ * on 2019/12/9
+ * 说明:
+ *
+ */
+data class HomeDataItem(
+    var imageUrl: String,
+    var title: String,
+    var price: Float,
+    var cny: String,
+    var rise: String
+)
--- app/src/main/java/com/ringle_al/wallet/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/ringle_al/wallet/ui/fragment/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.ringle_al.wallet.ui.fragment
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.ringle_al.common.base.BaseFragment
 import com.ringle_al.wallet.R
+import com.youth.banner.loader.ImageLoader
 import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
@@ -16,14 +20,45 @@ import kotlinx.android.synthetic.main.fragment_home.*
  */
 class HomeFragment : BaseFragment() {
     override fun setContentView(): Int = R.layout.fragment_home
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+//    private val mAdapter=
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+    override fun onStart() {
+        super.onStart()
+        initView()
+    }
+
+    private fun initView() {
+        //轮播图
+        val banners_path = listOf(
+            R.drawable.home_banner01,
+            R.drawable.home_banner02,
+            R.drawable.home_banner03
+        )
+        banner_home.setImages(banners_path)
+        banner_home.setImageLoader(object : ImageLoader() {
+            override fun displayImage(context: Context?, path: Any?, imageView: ImageView?) {
+                Glide.with(context!!.getApplicationContext())
+                    .load(path)
+                    .into(imageView!!);
+            }
+
+        })
+        banner_home.start()
+
+        //滚动通知栏
+        val notifys = listOf(
+            "Wallet已支持本次ETH伊斯坦布尔升级",
+            "关于币币交易减免手续费通知",
+            "Wallet新版本即将发布"
+        )
+        mqv_home_notify.startWithList(notifys as List<Nothing>?)
+
     }
 
 
+    override fun onStop() {
+        super.onStop()
+        banner_home.stopAutoPlay()
+    }
 }
